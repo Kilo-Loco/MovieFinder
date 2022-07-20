e57a2676390cbdf88acb27090d973c261bb299d7
Kilo Loco <kyle@kiloloco.com>
2022-07-20 03:16:22 -0700

    Working navigation

---
+++ app/src/main/java/com/example/moviefinder/APIService.kt
@@ -0,0 +1,135 @@
+package com.example.moviefinder
+
+import android.util.Log
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.moviefinder.ApiService.Companion.apiService
+import com.google.gson.annotations.SerializedName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Path
+import retrofit2.http.Query
+import java.lang.Exception
+
+data class Movie(
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("poster_path")
+    val posterPath: String,
+    @SerializedName("overview")
+    val overview: String
+) {
+    val fullPostPath: String
+        get() = "https://image.tmdb.org/t/p/w500$posterPath"
+}
+
+data class TopRatedMoviesResponse(
+    @SerializedName("results")
+    val movies: List<Movie>
+)
+
+data class VideoStreamPlatform(
+    @SerializedName("provider_name")
+    val name: String
+)
+
+data class VideoProviderRegion(
+    @SerializedName("flatrate")
+    val streamingPlatforms: List<VideoStreamPlatform>
+)
+
+data class  VideoProviderResponseResults(
+    @SerializedName("US")
+    val us: VideoProviderRegion?
+)
+
+data class VideoProviderResponse(
+    @SerializedName("results")
+    val results: VideoProviderResponseResults
+)
+
+interface ApiService {
+    @Headers("Content-Type: application/json;charset=utf-8")
+    @GET("movie/top_rated?api_key=$tmdbApiKey")
+    suspend fun topRatedMovies(): TopRatedMoviesResponse
+
+    @Headers("Content-Type: application/json;charset=utf-8")
+    @GET("movie/{movieId}/watch/providers?api_key=$tmdbApiKey")
+    suspend fun streamingPlatforms(
+        @Path("movieId")
+        movieId: String
+    ): VideoProviderResponse
+
+    companion object {
+        var apiService: ApiService? = null
+        fun getInstance(): ApiService {
+            if (apiService == null) {
+                apiService = Retrofit.Builder()
+                    .baseUrl("https://api.themoviedb.org/3/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+                    .create(ApiService::class.java)
+            }
+            return apiService!!
+        }
+    }
+}
+
+class MovieViewModel : ViewModel() {
+    private val _moviesList = mutableStateListOf<Movie>()
+    val moviesList: List<Movie>
+        get() = _moviesList
+
+    var selectedMovie: Movie? = null
+
+    private val _streamProviders = mutableStateListOf<VideoStreamPlatform>()
+    val streamProviders: List<VideoStreamPlatform>
+        get() = _streamProviders
+
+    fun getTopRatedMovies() {
+        viewModelScope.launch {
+            val apiService = ApiService.getInstance()
+            try {
+                _moviesList.clear()
+                _moviesList.addAll(apiService.topRatedMovies().movies)
+                _moviesList.forEach { Log.i("KILO", it.toString()) }
+            } catch (e: Exception) {
+                Log.e("kilo", "Error", e)
+            }
+        }
+    }
+
+    fun getStreamingProviders() {
+        viewModelScope.launch {
+            val apiService = ApiService.getInstance()
+            try {
+                _streamProviders.clear()
+
+                val streamingPlatforms = apiService
+                    .streamingPlatforms(selectedMovie!!.id)
+                    .results.us?.streamingPlatforms
+
+                if (streamingPlatforms != null) {
+                    _streamProviders.addAll(streamingPlatforms)
+                }
+                _streamProviders.forEach { Log.i("KILO", it.toString()) }
+            } catch (e: Exception) {
+                Log.e("kilo", "Error", e)
+            }
+        }
+    }
+
+    fun selectMovie(movie: Movie) {
+        selectedMovie = movie
+    }
+}
--- app/src/main/java/com/example/moviefinder/MainActivity.kt
+++ app/src/main/java/com/example/moviefinder/MainActivity.kt
@@ -1,62 +1,131 @@
 package com.example.moviefinder
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.Observer
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import coil.compose.rememberImagePainter
 import com.example.moviefinder.ui.theme.MovieFinderTheme
 
 @ExperimentalFoundationApi
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            MovieFinderTheme {
-                LazyVerticalGrid(
-                    cells = GridCells.Adaptive(150.dp),
-                    content = {
-                        items(50) { i ->
-                            Box(
-                                modifier = Modifier
-                                    .padding(4.dp)
-                                    .aspectRatio(0.75f)
-                                    .background(Color.Green)
-                            ) {
-                                Text(text = "Item $i")
-                            }
-                        }
-                    }
-                )
+            MaterialTheme {
+                AppNavigator()
             }
+        }
+    }
+
+    @Composable
+    fun AppNavigator() {
+        val navController = rememberNavController()
+        val vm = MovieViewModel()
+        
+        NavHost(navController = navController, startDestination = "topMovies") {
+            composable("topMovies") { MoviesView(navController = navController, vm = vm) }
+            composable("movieDetails") { MovieDetails(vm = vm) }
         }
     }
 }
 
+@ExperimentalFoundationApi
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MoviesView(navController: NavController, vm: MovieViewModel) {
+    LaunchedEffect(Unit, block = {
+        vm.getTopRatedMovies()
+    })
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Top Rated Movies") },
+                actions = {
+                    Button(onClick = { /*TODO*/ }) {
+                        Text("Login")
+                    }
+                }
+            )
+        }
+    ) {
+        LazyVerticalGrid(
+            cells = GridCells.Adaptive(150.dp),
+            content = {
+                items(vm.moviesList) { movie ->
+                    Image(
+                        painter = rememberImagePainter(movie.fullPostPath),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .padding(4.dp)
+                            .aspectRatio(0.75f)
+                            .clickable(onClick = {
+                                vm.selectMovie(movie)
+                                navController.navigate("movieDetails")
+                            })
+                    )
+                }
+            }
+        )
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    MovieFinderTheme {
-        Greeting("KILO")
+fun MovieDetails(vm: MovieViewModel) {
+
+    LaunchedEffect(Unit, block = {
+        vm.getStreamingProviders()
+    })
+    val selectedMovie = vm.selectedMovie!!
+    
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Row {
+                Text(selectedMovie.title)
+            } })
+        }
+    ) {
+        Column() {
+            Text(text = selectedMovie.overview, fontSize = 20.sp)
+            Spacer(modifier = Modifier.height(20.dp))
+
+            if (vm.streamProviders.isNotEmpty()) {
+                Text(text = "Streaming on:", fontWeight = FontWeight.Bold)
+                vm.streamProviders.forEach {
+                    Text(it.name)
+                }
+            } else {
+                Text(text = "Not available on streaming", fontWeight = FontWeight.Bold)
+            }
+        }
     }
 }
